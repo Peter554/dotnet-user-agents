3996bd24747e9cab4e039e315ac602a0ce358c5f
Peter Byfield <peter.byfield@enable.com>
2019-11-04 09:39:44 +0000

    UserAgent endpoint

--- WebAPI/Controllers/UserAgentController.cs
+++ WebAPI/Controllers/UserAgentController.cs
@@ -9,23 +9,53 @@ namespace WebAPI.Controllers
     {
         public IActionResult Get()
         {
-            var userAgent = Request.Headers["User-Agent"].ToString();
+            var userAgent = HttpContext.Request.Headers["User-Agent"].ToString();
+            var ip = HttpContext.Connection.RemoteIpAddress.ToString();
 
-            // TODO If null
+            if (string.IsNullOrWhiteSpace(userAgent))
+            {
+                return BadRequest("User agent could not be determined.");
+            }
+
+            if (string.IsNullOrWhiteSpace(ip))
+            {
+                return BadRequest("IP could not be determined.");
+            }
 
             var dd = new DeviceDetector(userAgent);
-
             dd.Parse();
 
-            var client = dd.GetClient()?.Match;
-            var os = dd.GetOs()?.Match;
-            var deviceName = dd.GetDeviceName();
-            var brandName = dd.GetBrandName();
-            var modelName = dd.GetModel();
+            var device = OrUnknown(dd.GetDeviceName(), "device");
+            var brand = OrUnknown(dd.GetBrandName(), "brand");
+            var model = OrUnknown(dd.GetModel(), "model");
 
-            // return Ok(client);
-            return Ok(os);
-            // return Ok($"{deviceName} {brandName} {modelName}");
+            var osMatch = dd.GetOs()?.Match;
+            var osName = OrUnknown(osMatch?.Name, "os");
+            var osVersion = OrUnknown(osMatch?.Version, "os-version");
+            var osPlatform = OrUnknown(osMatch?.Platform, "os-platform");
+
+            var clientMatch = dd.GetClient()?.Match;
+            var clientName = OrUnknown(clientMatch?.Name, "client");
+            var clientType = OrUnknown(clientMatch?.Type, "client-type");
+            var clientVersion = OrUnknown(clientMatch?.Version, "client-version");
+
+            ip = OrUnknown(ip, "ip-address");
+
+            return Ok($"" +
+                $"Device: {device} {brand} {model}\n" +
+                $"OS: {osName} {osVersion} {osPlatform}\n" +
+                $"Client: {clientName} {clientType} {clientVersion}\n" +
+                $"IP: {ip}");
+        }
+
+        private string OrUnknown(string s, string specifier)
+        {
+            if (string.IsNullOrWhiteSpace(s))
+            {
+                return $"unknown-{specifier}";
+            }
+
+            return s;
         }
     }
 }
