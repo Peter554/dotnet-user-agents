6859a8d3425e11e287704631ddbbc35e3e5850c2
Peter Byfield <peter.byfield@enable.com>
2019-11-04 09:03:32 +0000

    DeviceDetector.NET

---
+++ WebAPI/Controllers/UserAgentController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DeviceDetectorNET;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserAgentController : ControllerBase
+    {
+        public IActionResult Get()
+        {
+            var userAgent = Request.Headers["User-Agent"].ToString();
+
+            // TODO If null
+
+            var dd = new DeviceDetector(userAgent);
+
+            dd.Parse();
+
+            var client = dd.GetClient()?.Match;
+            var os = dd.GetOs()?.Match;
+            var deviceName = dd.GetDeviceName();
+            var brandName = dd.GetBrandName();
+            var modelName = dd.GetModel();
+
+            // return Ok(client);
+            return Ok(os);
+            // return Ok($"{deviceName} {brandName} {modelName}");
+        }
+    }
+}
